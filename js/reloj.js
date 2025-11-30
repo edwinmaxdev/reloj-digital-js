b7ee5ac90c5934d35202de02b2e59594244a9bfc
EdwinMaxDev <edwin789suarezb@gmail.com>
2025-11-30 17:38:15 -0500

    agregando codigos del reloj

---
+++ js/reloj.js
@@ -0,0 +1,40 @@
+    function mostrarHoras(){
+        let fechas = new Date();
+        let hr = agregarCeros(fechas.getHours());
+        let min = agregarCeros(fechas.getMinutes());
+        let seg = agregarCeros(fechas.getSeconds());
+
+        let horas = `${hr}:${min}:${seg}` ;
+
+        document.getElementById("horas").innerHTML = horas;
+        
+        
+
+
+        let meses = ["Ene", "feb", "mar", "abr", "may", "jun", "jul", "agos", "sept", "oct", "nov","dic"];
+        let dias = ["domingo", "lunes", "martes","miercoles", "jueves", "viernes", "sabado"];
+        
+        
+        let mes = meses[fechas.getMonth()];
+        let dia = dias[fechas.getDay()];
+        let fec = fechas.getDate();
+
+        let dias_Semana = `${mes},${fec} ${dia}`;
+
+        document.getElementById("semanas").innerHTML = dias_Semana;
+
+        
+
+
+    }
+    function agregarCeros(num){
+        if (num <= 0){
+            return "0"+num;
+        }else{
+            return num
+        }
+    }
+
+
+
+    setInterval(mostrarHoras, 1000);
