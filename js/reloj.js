a932242ccb4c9f3144f269afff82de8217893f75
edwinmaxdev <edwin789suarez@gmail.com>
2026-08-19 21:07:13 -0500

    Modificando el css

--- js/reloj.js
+++ js/reloj.js
@@ -3,17 +3,13 @@
         let hr = agregarCeros(fechas.getHours());
         let min = agregarCeros(fechas.getMinutes());
         let seg = agregarCeros(fechas.getSeconds());
-
         let horas = `${hr}:${min}:${seg}` ;
 
         document.getElementById("horas").innerHTML = horas;
-        
         document.getElementById("reloj").classList.toggle("animar");
-
 
         let meses = ["Ene", "feb", "Mar", "Abr", "May", "Jun", "jul", "agos", "Sept", "Oct", "Nov","Dic"];
         let dias = ["domingo", "lunes", "martes","miercoles", "jueves", "viernes", "sabado"];
-        
         
         let mes = meses[fechas.getMonth()];
         let dia = dias[fechas.getDay()];
@@ -22,15 +18,8 @@
         let dias_Semana = `${mes}, ${fec} ${dia}`;
 
         document.getElementById("semanas").innerHTML = dias_Semana;
-
-        
-
-
     }
     function agregarCeros(num){
         return num < 10 ? "0" + num :num;
     }
-
-
-
     setInterval(mostrarHoras, 1000);
